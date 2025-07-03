df9c4d83dd0488e719539777c03d49ead6170781
CCJulian <146324483+byeKunxix886@users.noreply.github.com>
2025-07-03 23:24:01 +0800

    Update a20.py

--- a20 身分證檢驗/a20.py
+++ a20 身分證檢驗/a20.py
@@ -1,12 +1,18 @@
-A_B = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-TO_NUM = [10,11,12,13,14,15,16,17,34,18,19,20,21,22,35,23,24,25,26,27,28,29,32,30,31,33]
-    
+id_map = { 'A':10,'B':11,'C':12,'D':13,'E':14,
+           'F':15,'G':16,'H':17,'I':34,'J':18,'K':19,
+           'L':20,'M':21,'N':22,'O':35,'P':23,'Q':24,
+           'R':25,'S':26,'T':27,'U':28,'V':29,'W':32,
+           'X':30,'Y':31,'Z':33
+}
+
+
+
 sum_id = 0
 identi = input()
 
 # print(TO_NUM[A_B.index(identi[0])]) # x -> index -> y[index] -> y
 
-num_firstletter = TO_NUM[A_B.index(identi[0])]
+num_firstletter = id_map[identi[0]]
 #print(num_firstletter) #debug: 拿到正確的字母所對應的數字 OK
 sum_id = (num_firstletter // 10) +  ((num_firstletter % 10) * 9) # /為精確商，小數點都會取，直到完全能得到被除數，商 = 被除數 ÷ 除數 。但通常我們要整數商//
 # print(sum_id) #Debug: sum_id 計算正確 NO 
